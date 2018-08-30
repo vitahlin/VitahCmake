15135373d055cf8d3626b09e0a46feaf9ed1801f
Vitah <linw1225@gmail.com>
2018-08-30 22:01:14 +0800

    Feat：cmake demo 2

---
+++ Demo_2/demo2.h
@@ -0,0 +1,2 @@
+
+#define MAX_NUM 10
---
+++ Demo_2/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include "demo2.h"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "This is Demo_2 " << endl;
+    cout << "Max num is " << MAX_NUM << endl;
+    return 0;
+}
