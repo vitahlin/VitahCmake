d8ec7d3f3489fd8d0af494c6c17a12bc2823a596
Vitah <linw1225@gmail.com>
2018-08-31 17:03:09 +0800

    Feat：同一目录多个文件示例

---
+++ Demo_3/main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include "demo3.h"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "This is Demo_3 " << endl;
+    cout << "Max Num is " << MAX_NUM << endl;
+    cout << "Double Max num is " << doubleNum(MAX_NUM) << endl;
+    return 0;
+}
