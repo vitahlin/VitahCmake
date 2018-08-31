39680e5b2dff604cd844cf99f3770bd9bdf5b68f
Vitah <linw1225@gmail.com>
2018-08-31 23:38:09 +0800

    Feat：同一目录多个文件示例

---
+++ Demo_4/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include "doubleNum.h"
+#include "powNum.h"
+using namespace std;
+
+#define MAX_NUM 3
+
+int main(int argc, char *argv[]) {
+    cout << "This is Demo_4 " << endl;
+    cout << "Max Num is " << MAX_NUM << endl;
+    cout << "Double is " << doubleNum(MAX_NUM) << endl;
+    cout << "Power is " << powNum(MAX_NUM, 2) << endl;
+    return 0;
+}
